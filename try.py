aa07b9fea60ebe44ffcba3bd9ec7fe9dcd2bf0c5
byislearner <1879840559@qq.com>
2024-06-02 22:35:05 +0800

    C4 first commit

--- try.py
+++ try.py
@@ -10,5 +10,5 @@ graph.add_edges_from({('A', 'B'): 1, ('B', 'C'): 2, ('C', 'D'): 3, ('D', 'A'): 4
 # 绘制有向图
 pos = nx.spring_layout(graph)  # 指定节点布局算法
 nx.draw_networkx(graph, pos, with_labels=True, node_color='lightblue', node_size=500, arrowstyle='->', arrowsize=15)
-plt.axis('off')  # 关闭坐标轴
-plt.show()
+#plt.axis('off')  # 关闭坐标轴
+#plt.show()
